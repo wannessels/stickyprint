69eccf2ec64e692c9af7730eea078b845b5096eb
Wannes Sels <wannes.sels@gmail.com>
2016-03-07 15:56:12 +0100

    auto-width op basis van inputtext

---
+++ src/main/java/Dimensions.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Wannes on 7/03/2016.
+ */
+public class Dimensions {
+    private int width;
+    private int height;
+
+    public Dimensions(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
--- src/main/java/StickyPrint.java
+++ src/main/java/StickyPrint.java
@@ -3,28 +3,31 @@
  */
 
 
-import jp.co.epson.upos.UPOSConst;
 import jpos.*;
-import jpos.events.*;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Random;
 
 
 public class StickyPrint {
 
     public static final String IMG_FILE = "imagewithtext.bmp";
+
+    //de volgende waarden zijn "good enough" voor 58mm papier, vooral gevonden door trail&error
+    public static final int FONT_SIZE = 200;
+    public static final int LIJN1_OFFSET = 40;
+    public static final int LIJN2_OFFSET = 42;
+
+    public static final Font FONT = new Font("SansSerif", Font.PLAIN, FONT_SIZE);
+    public static final int HEIGHT_58MM = 372;
+    public static final int HEIGHT_80MM = 372;
+    public static final int WIDTH_120MM = 768;
 
     public static void main(String[] args) throws JposException, IOException {
         System.setProperty("com.sun.media.jai.disableMediaLib", "true");
@@ -35,7 +38,7 @@ public class StickyPrint {
         ptr.setDeviceEnabled(true);
         //ptr.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\u001B|2uC       \u001B|bC\u001B|5CMr. Brawn\n");
         System.out.println(ptr.getRecLineWidth());
-        saveImage("Text","API?");
+        saveImage("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
         printImage(ptr);
         lineFeedAndCut(ptr);
 
@@ -51,40 +54,43 @@ public class StickyPrint {
 
     private static void printImage(POSPrinterControl114 ptr) throws JposException {
         ptr.setMapMode(POSPrinterConst.PTR_MM_METRIC);
-        // JavaPOS's code for Step5--END
-
-        //Output by the high quality mode
         ptr.setRecLetterQuality(true);
 
         if (ptr.getCapRecBitmap()==true) {
             ptr.transactionPrint(POSPrinterConst.PTR_S_RECEIPT, POSPrinterConst.PTR_TP_TRANSACTION);
             ptr.printBitmap(POSPrinterConst.PTR_S_RECEIPT, IMG_FILE, POSPrinterConst.PTR_BM_ASIS, POSPrinterConst.PTR_BM_LEFT);
             ptr.transactionPrint(POSPrinterConst.PTR_S_RECEIPT, POSPrinterConst.PTR_TP_NORMAL);
-
         }
-        //ptr.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\u001b|1B");
 
     }
 
-    static void saveImage(String lijn1, String lijn2) throws IOException {
-        //int imgHeight = 512; //voor 80mm papier
-        int imgHeight=372; // voor 58mm papier
-        int imgWidth=768; // ongeveer 12cm, cfr post-it
-        //int imgWidth=1024; //
+    static int getMinimumWidth(int currentWidth, String lijn) {
+        BufferedImage dummyImg = new BufferedImage(10000,400,BufferedImage.TYPE_BYTE_BINARY);
+        Graphics2D g2 = dummyImg.createGraphics();
+        FontRenderContext fontRenderContext = g2.getFontRenderContext();
+        Rectangle2D stringBounds = FONT.getStringBounds(lijn, fontRenderContext);
+        int width = Math.max(currentWidth,(int)stringBounds.getWidth()+20);
+        return width;
+    }
 
+    static void saveImage(String lijn1, String lijn2) throws IOException {
+        int imgHeight = HEIGHT_58MM;
+        int imgWidth = WIDTH_120MM;
+
+        imgWidth = getMinimumWidth(imgWidth, lijn1);
+        imgWidth = getMinimumWidth(imgWidth, lijn2);
         BufferedImage img = new BufferedImage(imgWidth,imgHeight,BufferedImage.TYPE_BYTE_BINARY);
 
         Graphics2D g2 = img.createGraphics();
         g2.setColor(Color.WHITE);
         g2.fillRect(0, 0, imgWidth, imgHeight);
         g2.setColor(Color.black);
-        Font font = new Font("SansSerif", Font.PLAIN, 200);
-        g2.setFont(font);
-        g2.drawString(lijn1,0 ,(imgHeight/2)-48);
-        g2.drawString(lijn2,0,imgHeight-48);
+        g2.setFont(FONT);
+        g2.drawString(lijn1,0 ,(imgHeight/2)- LIJN1_OFFSET);
+        g2.drawString(lijn2,0,imgHeight- LIJN2_OFFSET);
 
-        File outputfile = new File(IMG_FILE);
-        ImageIO.write(rotate90DX(img), "bmp", outputfile);
+        File outputFile = new File(IMG_FILE);
+        ImageIO.write(rotate90DX(img), "bmp", outputFile);
     }
 
     public static BufferedImage rotate90DX(BufferedImage src) {
@@ -95,7 +101,7 @@ public class StickyPrint {
         transform.rotate(Math.toRadians(90));
 
         BufferedImage result = new BufferedImage(newWidth,newHeight,BufferedImage.TYPE_BYTE_BINARY);
-        Graphics2D g2d = (Graphics2D) result.createGraphics();
+        Graphics2D g2d = result.createGraphics();
 
         g2d.drawImage(src, transform, null);
         g2d.dispose();
